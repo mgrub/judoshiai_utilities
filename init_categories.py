e3143fc3f2a60cc6681cebad210f9b5ee9aa280e
Maximilian Gruber <m.gruber@mailbox.org>
2024-11-10 11:46:12 +0100

    add argparse to init_categories

--- init_categories.py
+++ init_categories.py
@@ -3,14 +3,54 @@ import os
 import json
 import pandas
 from dbutils import JudoShiaiConnector
+import argparse
 
-template = "/home/maxwell/Desktop/judoshiai_test/masters_template.shi"
-output_directory = "/home/maxwell/Desktop/judoshiai_test/MASTERS_2023_TEST"
-competitors_xlsx = os.path.join(output_directory, "Meldungen_2023.xlsx")
-competitors_json = os.path.splitext(competitors_xlsx)[0] + ".json"
+# setup CLI
+# some defaults for argparse interface
+cwd = os.getcwd()
+default_template = os.path.join(cwd, "template.shi")
+default_competitors = os.path.join(cwd, "competitors.xlsx")
+
+# argparse options
+parser = argparse.ArgumentParser(
+    prog="ShiPrep",
+    description="Init Shiai-Template with Categories and Competitors",
+)
+
+parser.add_argument(
+    "-t",
+    "--template",
+    default=default_template,
+    help="shi-file to use as template DB",
+)
+parser.add_argument(
+    "-c",
+    "--competitors",
+    default=default_competitors,
+    help="xlsx-file with competitors",
+)
+parser.add_argument(
+    "-o",
+    "--output_dir",
+    default=cwd,
+    help="output directory to save prepared shi-DB",
+)
+parser.add_argument(
+    "-n",
+    "--name",
+    default="competition",
+    help="competition name (used for file naming)",
+)
+
+# parse CLI arguments
+args = parser.parse_args()
+template = args.template
+output_directory = args.output_dir
+competitors_xlsx = args.competitors
+competitors_json = os.path.join(output_directory, f"competitors_{args.name}.json")
+shi_path = os.path.join(output_directory, f"{args.name}.shi")
 
 # copy template
-shi_path = os.path.join(output_directory, "competition.shi")
 shutil.copy(template, shi_path)
 
 # connect to shi file
@@ -71,7 +111,7 @@ for i, row in df.iterrows():
         country = row["Nation"] if pandas.notna(row["Nation"]) else ""
 
         competitor = {
-            "ix": i+10,
+            "ix": i + 10,
             "last": row["Name"],
             "first": row["FirstName"],
             "club": row["Club"],
@@ -110,7 +150,6 @@ for i, row in df.iterrows():
             "coachid": competitor["coachid"],
         }
         db.insert_competitor(**competitor_db)
-
 
         competitors.append(competitor)
 
