7a789352f97c1b34339f98532eb611a8776b6b57
Maximilian Gruber <m.gruber@mailbox.org>
2024-12-01 19:07:36 +0100

    include teamsize in group score

--- result_summary.py
+++ result_summary.py
@@ -7,7 +7,7 @@ import os
 
 class ResultUtils:
 
-    def __init__(self, path):
+    def __init__(self, results_dir):
         self.category_pattern = re.compile("^(.*?) ([\+\-\d]*)kg$")
         self.medal_points = {
             1: 10,
@@ -16,9 +16,15 @@ class ResultUtils:
             4: 1,
             5: 1,
         }
+        self.results_dir = results_dir
 
-        with open(path, "r") as f:
+        results_path = os.path.join(results_dir, "results.json")
+        with open(results_path, "r") as f:
             self.results = json.load(f)
+        
+        competitors_path = os.path.join(results_dir, "competitors.json")
+        with open(competitors_path, "r") as f:
+            self.competitors = json.load(f)
 
         self.parse_results()
 
@@ -55,10 +61,13 @@ class ResultUtils:
                 for w in cat["competitors"]:
                     if w["pos"] <= pos_max:
                         print(f"  {w['pos']}. {w['first']} {w['last']} ({w['club']})")
+                print("")
 
     def group_summary(self, filter=""):
 
         pos_by_club = {}
+
+        clubsizes = self.teamsize(filter)
 
         print(f"\nGroup score {filter}")
         # get medals of each club
@@ -87,22 +96,49 @@ class ResultUtils:
             prev_points = points_by_club[0][1]
             place = 1
             for i, (club, points) in enumerate(points_by_club):
-                # same points should result in same place, but (e.g.) skip 4th place if we have 2x bronze
-                if points != prev_points:
-                    place = i+1
-                
-                print(f"  {place}. {club} ({points} points)")
-                prev_points = points
 
-                if place <= 3:
-                    if place in winners.keys():
-                        winners[place].append((club, points))
-                    else:
-                        winners[place] = [(club, points)]
+                if clubsizes[club] >= 3:
+                    # same points should result in same place, but (e.g.) skip 4th place if we have 2x bronze
+                    if points != prev_points:
+                        place = place+1  # TODO: not correct, but works for now
+                    
+                    print(f"  {place}. {club} ({points} points) (Teamsize: {clubsizes[club]})")
+                    prev_points = points
+
+                    if place <= 3:
+                        if place in winners.keys():
+                            winners[place].append((club, points))
+                        else:
+                            winners[place] = [(club, points)]
         else:
             print("  No points so far in this group.")
         
         return winners
+
+    def teamsize(self, filter=""):
+        
+        raw_competitors = []
+        raw_clubs = []
+
+        for c in self.competitors:
+            first = c["first"]
+            last = c["last"]
+            club = c["club"]
+            category = c["category"]
+
+            if re.fullmatch(filter, category):
+                raw_competitors.append((first, last, club))
+                raw_clubs.append(club)
+        
+        individual_competitors = list(set(raw_competitors))
+        clubs = list(set(raw_clubs))
+
+        clubsizes = {}
+        for club in clubs:
+            c_per_club = [c for c in individual_competitors if c[2] == club]
+            clubsizes[club] = len(c_per_club)
+
+        return clubsizes
 
     def generate_certificates(self, winners, template_path, cat_name):
         print(template_path)
@@ -144,9 +180,9 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "--file",
-        default="results/results.json",
-        help="path to result.json",
+        "--results-directory",
+        default="results",
+        help="path to results folder",
     )
 
     parser.add_argument(
@@ -176,28 +212,29 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # summarize results
-    ru = ResultUtils(args.file)
+    ru = ResultUtils(args.results_directory)
 
     if args.no_cat_results:
-        ru.winners_by_category(filter=args.cat_pattern, pos_max=3)
+        ru.winners_by_category(filter=args.cat_pattern, pos_max=4)
     
     if args.group_results:
         winners_men = ru.group_summary("Men.*")
         winners_women = ru.group_summary("Women.*")
 
         if args.group_cert_template != "":
-            ru.generate_certificates(winners_men, args.group_cert_template)
-            ru.generate_certificates(winners_women, args.group_cert_template)
+            ru.generate_certificates(winners_men, args.group_cert_template, "Männer")
+            ru.generate_certificates(winners_women, args.group_cert_template, "Frauen")
 
 
 
 
 # # testing
 # ru = ResultUtils(
-#     "/home/maxwell/Desktop/judoshiai_test/MASTERS_2023_TEST/results/results.json"
+#     "/home/maxwell/Desktop/judoshiai_test/MASTERS_2023_TEST/results/"
 # )
 # # ru.winners_by_category(pos_max=3)  # filter="Men|Women"
 # winners_men = ru.group_summary("Men.*")
+# ru.teamsize("Men.*")
 # winners_women = ru.group_summary("Women.*")
 
 # group_cert_template = "/home/maxwell/Desktop/Masters_2024/urkunde/urkunde_template.svg"
