bce30966cab1f9ac3a7591e1397cb05316eaaeb8
Maximilian Gruber <m.gruber@mailbox.org>
2025-03-21 13:25:39 +0100

    add generation of weight tickets to init script

--- init_shiai_db.py
+++ init_shiai_db.py
@@ -1,5 +1,8 @@
 import shutil
 import os
+import copy
+import shlex
+import subprocess
 import json
 import pandas
 from dbutils import JudoShiaiConnector_SQLITE
@@ -9,6 +12,7 @@ import argparse
 # some defaults for argparse interface
 cwd = os.getcwd()
 default_template = os.path.join(cwd, "template.shi")
+default_ticket = os.path.join(cwd, "weight_ticket.svg")
 default_competitors = os.path.join(cwd, "competitors.xlsx")
 
 # argparse options
@@ -31,7 +35,7 @@ parser.add_argument(
 )
 parser.add_argument(
     "-o",
-    "--output_dir",
+    "--output-dir",
     default=cwd,
     help="output directory to save prepared shi-DB",
 )
@@ -50,6 +54,21 @@ parser.add_argument(
     "--use-youth-categories",
     action='store_true',
     help="use categories of style <u12m>",
+)
+parser.add_argument(
+    "--create-tickets",
+    action='store_true',
+    help="create PDF weight tickets",
+)
+parser.add_argument(
+    "--ticket-template",
+    default=default_ticket,
+    help="svg-file to use as template for weight tickets",
+)
+parser.add_argument(
+    "--ticket-debug-mode",
+    action='store_true',
+    help="suppress actual pdf creation",
 )
 
 # parse CLI arguments
@@ -186,3 +205,95 @@ for i, row in df.iterrows():
 
 with open(competitors_json, "w", encoding="utf8") as f:
     json.dump(competitors, f, indent=1, ensure_ascii=False)
+
+# create tickets
+if args.create_tickets:
+    debug_mode = args.ticket_debug_mode
+
+    # read inkscape template
+    template = open(args.ticket_template, "r").read()
+
+    # make temporary folder
+    tmp_folder = os.path.join(cwd, "tmp")
+    if not os.path.exists(tmp_folder):
+        os.mkdir(tmp_folder)
+
+    # group by age and gender
+    groups = df.groupby(["Gender"]) # ["AgeCat", "Gender"]
+
+    for name, df_group in groups:
+
+        # sort
+        df_group = df_group.sort_values(["Club", "Name"]).fillna("")  # ["Weight", "Club", "Name"]
+
+        # keep track of produced temporary files
+        pdf_paths = []
+        svg_paths = []
+        inkscape_processes = []
+
+        print("Group: ", name)
+
+        # individuals
+        for i, row in df_group.iterrows():
+            
+            # build age category
+            age_raw = row["AgeCat"]
+            gender_raw = row["Gender"]
+            age_cat = age_cat_from_age_and_gender(age_raw, gender_raw)
+
+            # print info
+            print("{NAME}, {FNAME} ({CLUB}), {CAT}".format(
+                NAME=row['Name'], 
+                FNAME=row["FirstName"], 
+                CLUB=row["Club"], 
+                CAT=age_cat))
+
+            # prepare all necessary entries
+            replacements = {'__Name__': row['Name'],
+                            '__FirstName__': row['FirstName'],
+                            '__Club__': row['Club'],
+                            '__AgeCat__': age_cat,
+                            '__Nation__': row['Nation'],
+                            '__WeightCat__': row['WeightCat'],
+                            '__Weight__': row['Weight'] }
+
+            # substitue in inkscape template
+            tmp = copy.copy(template)
+            for placeholder, replacement in replacements.items():
+                tmp = tmp.replace(placeholder, replacement)
+            
+            # define output paths
+            svg_path = os.path.join(tmp_folder, "{ID:04d}.svg".format(ID=row["#"]))
+            pdf_path = os.path.join(tmp_folder, "{ID:04d}.pdf".format(ID=row["#"]))
+            svg_paths.append(svg_path)
+            pdf_paths.append(pdf_path)
+
+            # write svg
+            svg_file = open(svg_path, "w")
+            svg_file.write(tmp)
+            svg_file.close()
+
+            # convert inkscape to pdf
+            cmd_args = shlex.split("inkscape \"{SVG}\" --export-area-page --export-filename=\"{PDF}\"".format(SVG=svg_path, PDF=pdf_path))
+            if not debug_mode:
+                p = subprocess.Popen(cmd_args)
+                inkscape_processes.append(p)
+
+        # all processes were started in parallel, wait until all have finished before continuing 
+        exit_codes = [p.wait() for p in inkscape_processes]
+
+        # combine all pdf into single pdf for agecat
+        input_paths = ' '.join(pdf_paths)
+        output_path = os.path.join(cwd, '{NAME}_{CAT}.pdf'.format(NAME=args.name, CAT=name[0]))
+        s = "gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile={OUT} {IN}".format(OUT=output_path, IN=input_paths)
+        cmd_args = shlex.split(s)
+
+        if not debug_mode:
+            p = subprocess.Popen(cmd_args)
+            p.communicate()
+
+        # delete artifacts
+        for path in svg_paths + pdf_paths:
+            if os.path.exists(path):
+                os.remove(path)
+                
